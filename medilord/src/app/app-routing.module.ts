4318d7a02ead4d83a3b0aa192f0f77c360bff51b
rambhomkar <ram.bhomkar@gmail>
2021-05-19 23:25:26 +0530

    contact us page and home page images

--- medilord/src/app/app-routing.module.ts
+++ medilord/src/app/app-routing.module.ts
@@ -5,18 +5,23 @@ import { HomeComponent } from './home/home.component';
 import {ServicesComponent} from './services-page/services.component';
 import {HowItWorksComponent} from './how-it-works/how-it-works.component'
 import { CovidDoctorListComponent } from './covid-doctor-list/covid-doctor-list.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 const routes: Routes = [
-  {path:'', pathMatch:'full',redirectTo:'home'},
-  {path:'home',component:HomeComponent},
-  {path:'doctors',component:DoctorsListComponent},
-  {path:'services', component: ServicesComponent},
-  {path:'how-it-works', component:HowItWorksComponent},
-  {path:'covid-doctors-list', component:CovidDoctorListComponent}
+  {path:'home',component:HomeComponent , data:{title:'Medilord :: Home'}},
+  {path:'doctors',component:DoctorsListComponent, data:{title:'Medilord :: Doctor List'}},
+  {path:'services', component: ServicesComponent, data:{title:'Medilord :: Services'}},
+  {path:'how-it-works', component:HowItWorksComponent, data:{title:'Medilord :: How It Works'}},
+  {path:'covid-doctors-list', component:CovidDoctorListComponent, data:{title:'Medilord :: Covid-19 Doctor List'}},
+  {path:'contact-us', component:ContactUsComponent, data:{title:'Medilord :: Contact-Us'}},
+  {path:'', pathMatch:'full',redirectTo:'home'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled',
+
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- medilord/src/app/app.module.ts
+++ medilord/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DoctorsListComponent } from './doctors-list/doctors-list.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TransferHttpCacheModule} from '@nguniversal/common'
 import { SearchFilterPipe } from './helpers/search-filter.pipe';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './core/header/header.component';
@@ -13,6 +14,7 @@ import { ServicesComponent } from './services-page/services.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { CovidDoctorListComponent } from './covid-doctor-list/covid-doctor-list.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 
 @NgModule({
@@ -25,14 +27,16 @@ import { CovidDoctorListComponent } from './covid-doctor-list/covid-doctor-list.
     ServicesComponent,
     HowItWorksComponent,
     FooterComponent,
-    CovidDoctorListComponent
+    CovidDoctorListComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    TransferHttpCacheModule
   ],
   providers: [],
   bootstrap: [AppComponent]
